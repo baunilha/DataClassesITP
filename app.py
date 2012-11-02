0d00f1cf93974679897b6edff007851caaf45e1a
Matthew Epler <matthewepler@gmail.com>
2012-11-02 00:06:45 -0400

    update to index

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ categories = ['3D','Analog Craft','Animation','Art','Audio','Biology','Business'
 @app.route("/")
 def index():
 	# render the template, retrieve 'courses' from the database
+	semester = ["Fall", "2012"]
 	return render_template("main.html", courses=models.Course.objects())
 
 @app.route("/filter", methods=['POST'])
